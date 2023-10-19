5a825d3db5148aea676dec1b1581a3c645c5cf80
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-19 13:32:34 +0800

    search

---
+++ backend/routes/auth.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const {registerUser, loginUser, logout, forgotPassword, resetPassword, updatePassword, getUserProfile, updateProfile, allUsers, getUserDetails} = require('../controllers/authController');
+
+const {isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth')
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/logout', logout);
+router.post('/password/forgot', forgotPassword);
+router.put('/password/reset/:token', resetPassword);
+router.put('/password/update', isAuthenticatedUser, updatePassword)
+router.get('/me', isAuthenticatedUser, getUserProfile)
+router.put('/me/update', isAuthenticatedUser, updateProfile)
+
+router.get('/admin/users', isAuthenticatedUser, allUsers)
+router.get('/admin/user/:id',isAuthenticatedUser, getUserDetails )
+module.exports = router;
